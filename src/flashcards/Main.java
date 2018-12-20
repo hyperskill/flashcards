642e4f24e2ff6c92c0ae154fe327eba77b2cceff
aleks <alekseyvy@gmail.com>
2018-12-20 19:34:27 +0300

    Stage 5 Main_Menu.

--- src/flashcards/Main.java
+++ src/flashcards/Main.java
@@ -2,6 +2,6 @@ package flashcards;
 
 public class Main {
     public static void main(String[] args) {
-        cards.cardInput();
+        cards.mainMenu();
     }
 }
--- src/flashcards/cards.java
+++ src/flashcards/cards.java
@@ -2,52 +2,48 @@ package flashcards;
 
 import java.util.*;
 
-import static java.lang.System.*;
-
 public class cards {
-
-    protected static void cardInput(){
-        Scanner input = new Scanner(in);
-        //ask user for input number of cards
-        out.println("Input the number of cards: ");
-        int numOfCards = input.nextInt();
-        //HashMap's for storing cards and definitions
-        Map<String, String> cardToDefinition = new HashMap();
-        Map<String, String> definitionToCard = new HashMap();
-        //loop for populating HashMap with values
-        for(int i = 0; i < numOfCards; i++){
-            out.println("The card #" + i +":");
-            String cardValue = input.nextLine();
-            out.println("The definition of the card #" + i +":");
-            String defValue = input.nextLine();
-            if(cardToDefinition.containsKey(cardValue) || definitionToCard.containsKey(defValue)){
-                out.println("Please reenter new value");
-                if (i > 0) {
-                    i--;
-                } else {
-                    i = 0;
-                }
-            } else {
-                cardToDefinition.put(cardValue, defValue);
-                definitionToCard.put(defValue, cardValue);
-            }
+    //main subroutine for choosing menu items
+    protected static void mainMenu(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input the action (add, remove, import, export, ask, exit): ");
+        String menuChoice = scan.nextLine();
+        switch (menuChoice){
+            case "add":
+                break;
+            case "remove":
+                break;
+            case "import":
+                break;
+            case "export":
+                break;
+            case "ask":
+                break;
+            case "exit":
+                exit();
+                break;
         }
-        //loop for answering questions
-        cardToDefinition.forEach((key, value) ->{
-            System.out.println("Print the definition of: " + key);
-            String userAnswer = input.nextLine();
-            if (!value.equals(userAnswer) && definitionToCard.containsKey(userAnswer)){
-                System.out.println("Wrong answer (the correct one is \"" + value +"\", you've just written a definition of \"" + definitionToCard.get(userAnswer) + "\" card). Print the definition of \"" + definitionToCard.get(userAnswer) + "\":");
-                userAnswer = input.nextLine();
-                if (cardToDefinition.containsKey(userAnswer)){
-                    out.println("Correct");
-                } else {
-                    System.out.println(cardToDefinition.get(userAnswer));
-                }
-            } else {
-                System.out.println("Correct");
-            }
-
-        });
     }
+    //subroutine for adding cards
+    protected static void addCard(){
+
+    }
+    //subroutine for removing cards
+    protected  static void removeCard(){
+    }
+    //subroutine for import
+    protected static void importCard(){
+    }
+    //subroutine for export
+    protected static void exportCard(){
+    }
+    //subroutine for asking user
+    protected static void askCard(){
+    }
+    //exit subroutine
+    protected static void exit(){
+        System.out.println("Bye bye!");
+        System.exit(0);
+    }
+
 }
