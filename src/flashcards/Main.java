2dcf082b0950de43e1825addf3b0b6a5818c2677
Petriankin <sergei_petriankin@epam.com>
2019-02-02 18:55:37 +0300

    Answers analytics implement

--- src/flashcards/Main.java
+++ src/flashcards/Main.java
@@ -1,29 +1,43 @@
 package flashcards;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Input the number of cards:");
-        int amount = sc.nextInt();
-        String[] cards = new String[amount];
-        String[] definitions = new String[amount];
+        System.out.print("Input the number of cards:\n");
+        int amount = Integer.parseInt(sc.nextLine());
+        Map<String, String> cardToDefinition = new HashMap<>();
+        Map<String, String> definitionToCard = new HashMap<>();
+
         for (int i = 0; i < amount; i++) {
             System.out.printf("The card #%d:\n", i + 1);
-            cards[i] = sc.next();
+            String card = sc.nextLine();
+
             System.out.printf("The definition of the card #%d:\n", i + 1);
-            definitions[i] = sc.next();
+            String definition = sc.nextLine();
+
+            cardToDefinition.put(card, definition);
+            definitionToCard.put(definition, card);
         }
-        for (int i = 0; i < amount; i++) {
-            System.out.printf("Print the definition of \"%s\":\n", cards[i]);
+        for (Map.Entry<String, String> entry : cardToDefinition.entrySet()) {
+            System.out.printf("Type the definition of \"%s\":\n", entry.getKey());
             String answer = sc.next();
-            if (definitions[i].equals(answer)) {
+
+            if (entry.getValue().equals(answer)) {
                 System.out.print("Correct answer. ");
                 continue;
+            } else if (definitionToCard.containsKey(answer)) {
+                System.out.printf("Wrong answer (the correct one is \"%s\")," +
+                                " you've just written a definition of \"%s\" card).\n",
+                        entry.getValue(),
+                        definitionToCard.get(answer));
             } else {
-                System.out.printf("Wrong answer (the correct one is \"%s\").\n", definitions[i]);
+                System.out.printf("Wrong answer (the correct one is \"%s\").\n", entry.getValue());
             }
+
         }
     }
 }
