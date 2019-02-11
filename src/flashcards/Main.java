b23ad7e0bd1383ffdd9de8139950b33054538d87
Y2koV <y2kov.d@gmail.com>
2019-02-11 13:55:15 +0300

    Stage 3: Create a set of cards

--- src/flashcards/Main.java
+++ src/flashcards/Main.java
@@ -1,7 +1,26 @@
 package flashcards;
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input the number of cards:");
+        int numOfCards = scanner.nextInt();
+        String[] cards = new String[numOfCards];
+        String[] definition = new String[numOfCards];
+        for (int i=0; i<numOfCards; i++) {
+            System.out.println("The card #" + (i+1) + ":");
+            cards[i] = scanner.next();
+            System.out.println("The definition of the card #" + (i+1) + ":");
+            definition[i] = scanner.next();
+        }
+        for (int n=0; n<numOfCards; n++) {
+            System.out.println("Print the definition of (" + cards[n] + "):");
+            String answer = scanner.next();
+            if (answer.equals(definition[n])) {
+                System.out.print("Correct answer. ");
+            } else {
+                System.out.print("Wrong answer (the correct one is " + definition[n] + ").");
+            }
+        }
     }
 }
