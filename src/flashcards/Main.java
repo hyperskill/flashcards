f7c1f1ef1dd5a19105e6467d7d000cdb6efd2f52
Y2koV <y2kov.d@gmail.com>
2019-02-12 18:54:50 +0300

    Stage 5: Work with files

--- src/flashcards/Main.java
+++ src/flashcards/Main.java
@@ -1,46 +1,59 @@
 /*
-There is a common situation that the answer is wrong for the given card but it's correct for another card. Let's consider situations like this.
+Stage 4: There is a common situation that the answer is wrong for the given card but it's correct for another card. Let's consider situations like this.
 Remove array-based storage. Use two maps (Map<String, String>): cardToDefinition and definitionToCard. If the definition is wrong but it is presented
 in definitionToCard, output the original card.
 When the user tries to add a duplicated card or a definition, forbid it. For now you are able to implement this without a try catch construction.
 Use the rule: if you can avoid exception-based logic, avoid it!
+
+Stage 5: Improve the application’s interactivity. Ask the user for an action and do it.
+Support these actions:
+add a card: add,
+remove a card: remove,
+load cards from file ("deserialization"): import,
+save cards to file ("serialization"): export,
+ask for definition of some random cards: ask,
+exit the program: exit.
+You can use the following file format. The file consists of pairs of lines. The first line of each pair is a card, and the second line is a definition of the card.
  */
 
 package flashcards;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Input the number of cards:");
-        int numOfCards = scanner.nextInt();
-        Map<String, String> cardToDefinition = new HashMap<>();
-        Map<String, String> definitionToCard = new HashMap<>();
+        usingFlashcards();
+    }
 
-        //creating 2 Map's with cards and definition (and opposite)
-        for (int i=0; i<numOfCards; i++) {
-            System.out.println("The card #" + (i+1) + ":");
-            String userCard = scanner.next();
-            System.out.println("The definition of the card #" + (i+1) + ":");
-            String userDefinition = scanner.next();
-            if (cardToDefinition.containsKey(userCard) || cardToDefinition.containsValue(userDefinition)) {
-                System.out.println("Don't use a duplicated card or a definition");
-                return;
-            } else {
-                cardToDefinition.put(userCard, userDefinition);
-                definitionToCard.put(userDefinition, userCard);
+    private static void usingFlashcards() {
+        Scanner scanner = new Scanner(System.in);
+        Flashcards flashcards = new Flashcards();
+        String action;
+        do {
+            System.out.println("Input the action (add, remove, import, export, ask, exit):");
+            action = scanner.next();
+            switch (action) {
+                case "add":
+                    flashcards.add();
+                    break;
+                case "remove":
+                    flashcards.remove();
+                    break;
+                case "ask":
+                    flashcards.ask();
+                    break;
+                case "import":
+                    flashcards.importCards();
+                    break;
+                case "export":
+                    flashcards.exportCards();
+                    break;
+                case "exit":
+                    break;
+                default:
+                    System.out.println("Wrong action!");
+                    break;
             }
-        }
-        // checking user answer
-        for (Map.Entry<String, String> card : cardToDefinition.entrySet()) {
-            System.out.println("Print the definition of (" + card.getKey() + "):");
-            String answer = scanner.next();
-            if (answer.equals(card.getValue())) {
-                System.out.print("Correct answer. ");
-            } else if (cardToDefinition.containsValue(answer)) {
-                System.out.print("Wrong answer (the correct one is " + card.getValue()+ ", you've just written a definition of " + definitionToCard.get(answer) + " card).");
-            } else {
-                System.out.print("Wrong answer (the correct one is " + card.getValue() + ").");
-            }
-        }
+        } while (!action.equals("exit"));
+        scanner.close();
+        System.out.println("Bye bye!");
     }
 }
